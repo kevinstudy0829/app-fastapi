0fd258da3dfb02c41253b7a2df3e65380e917186
kevin <kevinstudy0829@gmail.com>
2026-06-07 10:09:06 +0000

    更新版本號至 v5

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ def read_root():
         "DB_name": os.getenv("DATABASE_USER"),
         "DB_pass": os.getenv("DATABASE_PASSWORD"),
         "api_key": os.getenv("API_KEY"),
-        "version": "v4"
+        "version": "v5"
     }
 
 # 定義一個帶參數的路由
